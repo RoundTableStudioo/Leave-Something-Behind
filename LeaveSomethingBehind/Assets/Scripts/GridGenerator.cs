92b5d424b10a497732960ba17f81937c5a94fe91
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2022-10-14 17:52:11 +0200

    Añadido terreno propio a la generación procedural

--- LeaveSomethingBehind/Assets/Scripts/GridGenerator.cs
+++ LeaveSomethingBehind/Assets/Scripts/GridGenerator.cs
@@ -4,8 +4,10 @@ using UnityEngine.Tilemaps;
 
 namespace RoundTableStudio
 {
+
     public class Cell {
-        public bool IsBush;
+        public bool IsRock;
+        public bool IsFlower;
         public bool IsTreeBottom;
         public bool IsTreeTop;
     }
@@ -18,16 +20,18 @@ namespace RoundTableStudio
         [Range(0, 1)]
         public float Scale;
         [Range(0, 1)]
-        public float BushDensity;
+        public float FlowerDensity;
+        [Range(0, 1)] 
+        public float RockDensity;
         [Range(0, 1)] 
         public float TreeDensity;
 
-        [Header("Tiles")] 
-        public Tilemap BushTileMap;
+        [Header("Tiles")]
         public Tilemap GrassTileMap;
-        public Tilemap TreeTileMap;
-        public Tile GrassTile;
-        public Tile BushTile;
+        public Tilemap DecorationTileMap;
+        public Tilemap StructureTileMap;
+        public Tile[] GrassTile;
+        public Tile[] RockTiles;
         public Tile[] TreeTiles;
 
         private Cell[,] _grid;
@@ -45,22 +49,24 @@ namespace RoundTableStudio
             for (int y = 0; y < GridHeight; y++) {
                 for (int x = 0; x < GridWidth; x++) {
 
-                    float bushNoiseValue = Mathf.PerlinNoise(y * Scale + xOffset, x * Scale + yOffset);
+                    float flowerNoiseValue = Mathf.PerlinNoise(y * Scale + xOffset, x * Scale + yOffset);
+                    float rockNoiseValue = Mathf.PerlinNoise(y * Scale + xOffset, x + Scale * yOffset);
                     float treeNoiseValue = Mathf.PerlinNoise(y * Scale + xOffset, x * Scale * yOffset);
 
                     Cell cell = new Cell();
 
                     if (x != 0 && y != 0) {
-                        if (!_grid[x, y - 1].IsTreeBottom)
-                            cell.IsBush = bushNoiseValue < BushDensity;
-                        if (!cell.IsBush && !cell.IsTreeTop && !cell.IsTreeBottom && !_grid[x, y - 1].IsTreeBottom)
+                        if (!_grid[x, y - 1].IsTreeBottom) {
+                            cell.IsFlower = flowerNoiseValue < FlowerDensity;
+
+                            if (!cell.IsFlower)
+                                cell.IsRock = rockNoiseValue < RockDensity;
+                        }
+                        if (!cell.IsFlower && !cell.IsRock && !_grid[x, y - 1].IsTreeBottom && !cell.IsTreeTop && !cell.IsTreeBottom)
                             cell.IsTreeBottom = treeNoiseValue < TreeDensity;
                     }
-                    
-                    
 
                     _grid[x, y] = cell;
-                    
                 }
             }
         }
@@ -70,20 +76,36 @@ namespace RoundTableStudio
                 for (int x = 0; x < GridWidth; x++) {
                     Cell cell = _grid[x, y];
                     Vector3Int pos = new Vector3Int(-x + GridWidth / 2, -y + GridHeight / 2, 0);
+                    
+                    int rotationAmount = 0;
+                    int rotation = Random.Range(-1, 1);
 
-                    if (cell.IsBush) 
-                        BushTileMap.SetTile(pos, BushTile);
+                    if (rotation == -1) rotationAmount = -90;
+                    else if (rotation == 1) rotationAmount = 90;
+
+                    if (cell.IsFlower) {
+                        int flowerNum = Random.Range(1, 3);
+                        
+                        DecorationTileMap.SetTile(pos, GrassTile[flowerNum]);
+                        DecorationTileMap.SetTransformMatrix(pos, Matrix4x4.Rotate(Quaternion.Euler(0f, 0f, rotationAmount)));
+                    }
+                    if (cell.IsRock) {
+                        int rockNum = Random.Range(0, 2);
+
+                        DecorationTileMap.SetTile(pos, RockTiles[rockNum]);
+                        DecorationTileMap.SetTransformMatrix(pos, Matrix4x4.Rotate(Quaternion.Euler(0f, 0f, rotationAmount)));
+                    }
                     if (cell.IsTreeBottom) {
                         if (x != GridWidth - 1 && y != GridHeight - 1) {
 
-                            TreeTileMap.SetTile(pos, TreeTiles[0]);
+                            StructureTileMap.SetTile(pos, TreeTiles[0]);
 
-                            TreeTileMap.SetTile(pos + new Vector3Int(0, 1, 0), TreeTiles[1]);
+                            StructureTileMap.SetTile(pos + new Vector3Int(0, 1, 0), TreeTiles[1]);
                             _grid[x, y + 1].IsTreeTop = true;
                         }
                     }
                     
-                    GrassTileMap.SetTile(pos, GrassTile);
+                    GrassTileMap.SetTile(pos, GrassTile[0]);
                     
                     yield return new WaitForSeconds(0.005f);
                 }
