7e31745018fcdf2a67d6efdea8fc0cb20589f4cb
David Antonio Vélez Rebollo <david_velezreb@hotmail.com>
2022-10-17 17:22:40 +0200

    Información de los Items

---
+++ LeaveSomethingBehind/Assets/Scripts/Objects/Item.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace RoundTableStudio.Items
+{
+    [CreateAssetMenu(fileName = "Item", menuName = "Item")]
+    public class Item : ScriptableObject {
+        [Header("General")]
+        [Tooltip("Icon of the item shown at the UI")]
+        public Sprite Icon;
+        [Tooltip("Name of the item")] 
+        public string Name;
+        [Tooltip("Description of the item")] 
+        [Multiline]
+        public string Description;
+    }
+}
